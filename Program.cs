44c5fa45fc7e1008939df526150890db472bf3a0
Mohamedbshr05 <121199696+Mohamedbshr05@users.noreply.github.com>
2022-12-24 18:23:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+class project 
+{
+    static public void Main()
+    {
+        int a, b, i, j, flag;
+
+        Console.Write("Enter a number please: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the second  number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Prime numbers between " + a + " and " + b + " are: ");
+        if (a == 1)
+        {
+            Console.Write(a + " ");
+            a++;
+            if (b >= 2)
+            {
+                Console.Write(a + " ");
+                a++;
+            }
+        }
+        if (a == 2)
+        {
+            Console.Write(a + " ");
+        }
+        if (a % 2 == 0)
+        {
+            a++;
+        }
+        for (i = a; i <= b; i = i + 2)
+        {
+            flag = 1;
+            for (j = 2; j * j <= i; ++j)
+            {
+                if (i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+            {
+                Console.Write(i + " ");
+            }
+            //iam mohamed bashiir mohamed section 18 group 3//
+        }
+    }
+}
